a834b6ce3c536f5bd24fcb02731926b7c1529e4f
Grigorii Orlov <utrom@MacBook-Air-Grigorii.local>
2022-03-26 12:15:35 +0400

    test cases added

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # bmi-component
+
+## file bmi-web-component.js   
+contains bmi-component web component
+
+###usage
+``` 
+    <bmi-score class="trr" name="Mr Nash" score="88.89"></bmi-score>
+    <script src="bmi-web-component.js"></script>
+```
+
+
+You can find test cases in the 'test' directory
--- bmi-web-component.js
+++ bmi-web-component.js
@@ -1,10 +1,12 @@
 const bmiTemplate = document.createElement('template');
 
-const makeTemplate = (name = "", score = 0) => {
+const makeTemplate = (name, score, width) => {
     bmiTemplate.innerHTML = `
     <style>
     .bmi-wrapper {
         margin: 60px;
+        width: ${width || 'initial'};
+        min-width: 300px;
     }
     h3, h2 {
       text-align: center;
@@ -21,10 +23,14 @@ const makeTemplate = (name = "", score = 0) => {
       margin-left: calc(${+score / 1.2}% - 10px);
     }
     .range-item {
+        transition: .2s;
         background-color: lightgray;
         height: 16px;
         float: left;
         box-sizing: border-box;
+    }
+    .range-item:hover {
+        opacity: .9;
     }
     .range-item p {
         color: steelblue;
@@ -39,6 +45,9 @@ const makeTemplate = (name = "", score = 0) => {
         border-right: 2px solid white;
         width: 25%;
     }
+    .underweight-range.active {
+        background-color: darkred;
+    }
     .normal-range {
         width: 50%;
     }
@@ -47,10 +56,13 @@ const makeTemplate = (name = "", score = 0) => {
         border-left: 2px solid white;
         width: 25%;
     }
+    .overweight-range.active {
+        background-color: darkred;
+    }
     </style>
     <div class="bmi-wrapper">
-    <h3>${name}'s BMI</h3>
-    <h2>${score}</h2>
+    ${name ? `<h3>${name}'s BMI</h3>` : ''}
+    ${score ? `<h2>${score}</h2>` : ''}
     <div class="range-wrapper">
         <div class="arrow-down" ></div>
         <div class="range">
@@ -70,39 +82,25 @@ const makeTemplate = (name = "", score = 0) => {
 `
 }
 
-// bmiTemplate.innerHTML = `
-//     <style>
-//     .arrow-down {
-//       width: 0;
-//       height: 0;
-//       border-left: 10px solid transparent;
-//       border-right: 10px solid transparent;
-//       border-top: 20px solid steelblue;
-//     }
-//     </style>
-//     <div class="bmi-wrapper">
-//     <h3></h3>
-//     <div class="range-wrapper">
-//         <div class="arrow-down" />
-//         <div class="range">
-//             <div class="underweight-range" />
-//             <div class="normal-range" />
-//             <div class="overweight-range" />
-//         </div>
-//
-//     </div>
-//     </div>
-// `
-
 class BMI extends HTMLElement {
+    static get tag() {
+        return "bmi-score";
+    }
+
     constructor() {
         super();
+        const name = (this.getAttribute('name'));
+        let score = (this.getAttribute('score')) || 0;
+        const width = (this.getAttribute('width'));
+
+        if (score < 0) score = 0;
+        if (score > 120) score = 120;
 
         this.attachShadow({ mode: "open" });
-        makeTemplate(this.getAttribute('name'), this.getAttribute('score'))
+        makeTemplate(name, score, width)
         this.shadowRoot.appendChild(bmiTemplate.content.cloneNode(true));
     }
 
 }
 
-window.customElements.define('bmi-score', BMI);
+window.customElements.define(BMI.tag, BMI);
